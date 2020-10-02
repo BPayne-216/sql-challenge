b9da7a6f11469815f926b355a0ba3443d9d4d35a
Robert Payne <rjp5473@yahoo.com>
2020-10-01 23:04:10 -0400

    changing config

--- EmployeeSQL/config.py
+++ EmployeeSQL/config.py
@@ -1,2 +1,1 @@
-#Enter sql log in below.
-sql = "postgresql+psycopg2://postgres:ziggyboy216@localhost:5432/employees_db"
+sql_entry = 'postgresql+psycopg2://postgres:ziggyboy216@localhost:5432/employees_db'
